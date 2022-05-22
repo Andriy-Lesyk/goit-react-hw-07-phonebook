1253e8f6bb2a69d9102be89eb3933f2a0d4b44de
Андрій <74345435+Andriy-Lesyk@users.noreply.github.com>
2022-05-22 11:12:37 +0300

    Виправляє POST запит

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -25,13 +25,16 @@ export const addContacts = createAsyncThunk(
     try {
       const contacts = await fetch(
         `https://6284af5d3060bbd3473d4a1e.mockapi.io/Contacts`,
-        { method: 'POST' }
+        {
+          method: 'POST',
+          body: JSON.stringify(contact),
+          headers: { 'Content-Type': 'application/json' },
+        }
       );
       if (!contacts.ok) {
         return rejectWithValue(contacts.status);
       }
       const data = await contacts.json();
-
       return fulfillWithValue(data);
     } catch (error) {
       throw rejectWithValue(error.message);
